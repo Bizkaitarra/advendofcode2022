12e2d7b1deca9bdb949d126c0577b59d15c0c540
Jon González Garrido <jon.gonzalez@atrapalo.com>
2022-12-03 16:10:09 +0100

    Add test input with file

--- tests/Day1/CaloriesCounterTest.php
+++ tests/Day1/CaloriesCounterTest.php
@@ -12,22 +12,8 @@ class CaloriesCounterTest extends TestCase
      * @test
      */
     public function assertExampleReturnsCorrectCalories() {
-        $calories =
-            "1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
+        $caloriesFile = __DIR__ . '/firstPartExampleInput.txt';
         $caloriesCounter = new CaloriesCounter();
-        $this->assertEquals(24000,$caloriesCounter->__invoke($calories));
+        $this->assertEquals(24000,$caloriesCounter->__invoke(file_get_contents($caloriesFile)));
     }
 }
